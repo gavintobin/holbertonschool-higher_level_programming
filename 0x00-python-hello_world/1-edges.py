304e29b01f190da90af93197e2d2b5711d94c2c2
gavintobin <gavintobin714@gmail.com>
2023-01-06 09:16:14 -0800

    try1

---
+++ 0x00-python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+word = "Holberton"
+word_first_3 = [:3]
+word_last_2 = [-2:]
+middle_word = [0:8]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
