fbeaada38368d0365cb847ec4d4895b95f0e2ada
gavintobin <gavintobin714@gmail.com>
2023-01-09 11:59:33 -0800

    tyry1

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    numarg = len(sys.argv) - 1
+    if numarg == 1:
+        print('{} arguement:'.format(numarg), end='\n')
+    else:
+        print('{} arguements:'.format(numarg))
+    for i in range(1, numarg + 1):
+        print('{}: {}'.format(i, sys.argv[i]))
