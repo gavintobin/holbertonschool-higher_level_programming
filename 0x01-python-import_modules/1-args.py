44f27e175de1d1099ab26b8b66fc01fe8e8de15e
gavintobin <gavintobin714@gmail.com>
2023-01-09 12:00:40 -0800

    tyry2

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == '__main__':
     numarg = len(sys.argv) - 1
     if numarg == 1:
-        print('{} arguement:'.format(numarg), end='\n')
+        print('{} arguement:'.format(numarg))
     else:
         print('{} arguements:'.format(numarg))
     for i in range(1, numarg + 1):
