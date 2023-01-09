fb17f5a1ef5b29b5a422ed0f5d29c127c9af54b1
gavintobin <gavintobin714@gmail.com>
2023-01-09 08:39:33 -0800

    added newline againm

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i == 8 and j == 9:
-            print('{:d}{:d}'.format(i, j), end ='\n ')
+            print('{:d}{:d}'.format(i, j), end='\n')
         elif i < j:
             print('{:d}{:d}, '.format(i, j), end='')
