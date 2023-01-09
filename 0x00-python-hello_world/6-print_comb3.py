9eb1a3ba98138b1951531204f82d125b0e174bdd
gavintobin <gavintobin714@gmail.com>
2023-01-09 08:37:11 -0800

    added coma

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i < j:
+        if i == 8 and j == 9:
+            print('{:d}{:d}'.format(i, j), end =' ')
+        elif i < j:
             print('{:d}{:d}, '.format(i, j), end='')
