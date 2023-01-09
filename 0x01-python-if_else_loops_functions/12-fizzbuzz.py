19604dd502b215966b5d0629c2d87f55a0e565a4
gavintobin <gavintobin714@gmail.com>
2023-01-09 10:19:26 -0800

    fixed range

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 15 == 0:
             print('FizzBuzz'.format(), end=' ')
         elif i % 5 == 0:
