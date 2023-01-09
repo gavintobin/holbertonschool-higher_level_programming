d189c7d31e35c5e021fcf51140eb2e668a6b340e
gavintobin <gavintobin714@gmail.com>
2023-01-09 10:17:49 -0800

    fixed order

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 100):
-        if i % 3 == 0:
-            print('Fizz'.format(), end=' ')
+        if i % 15 == 0:
+            print('FizzBuzz'.format(), end=' ')
         elif i % 5 == 0:
             print('Buzz'.format(), end=' ')
-        elif i % 15 == 0:
-            print('FizzBuzz'.format(), end=' ')
+        elif i % 3 == 0:
+            print('Fizz'.format(), end=' ')
         else:
             print('{}'.format(i), end=" ")
