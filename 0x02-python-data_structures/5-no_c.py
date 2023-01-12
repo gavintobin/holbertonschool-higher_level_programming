39f375e4004353d327d25bbc232f7965bd3fd638
gavintobin <gavintobin714@gmail.com>
2023-01-12 09:22:26 -0800

    fixed code

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.translate({ord('c'): None, ord('C'): None}))
+    my_string = (my_string.translate({ord('c'): None, ord('C'): None}))
+    print(my_string)
