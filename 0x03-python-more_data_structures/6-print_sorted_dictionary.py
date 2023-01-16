6b59df0f41f55827f35ef19c2e23047355b2d553
gavintobin <gavintobin714@gmail.com>
2023-01-16 10:28:08 -0800

    try1

---
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for key, val in sorted(a_dictionary.items()):
+        print(key, ':', val, sep='')
+    return sorted(a_dictionary.items())
