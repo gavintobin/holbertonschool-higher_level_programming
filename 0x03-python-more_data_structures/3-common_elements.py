b1d86421980dc8fdf2a14e0f93520e4ff546b3a1
gavintobin <gavintobin714@gmail.com>
2023-01-16 09:58:25 -0800

    try1

--- 0x03-python-more_data_structures/3-common_elements.py
+++ 0x03-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    one = set(set_1)
-    two = set(set_2)
-
-    if (one & two):
-        print(one & two)
+    rees = set_1.intersection(set_2)
+    return rees
