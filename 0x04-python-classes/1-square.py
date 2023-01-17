e674ef4f4e837eb97c99820c895d4520cfe7e149
gavintobin <gavintobin714@gmail.com>
2023-01-16 18:33:46 -0800

    fixed space where not needed

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         """initializes square with size"""
         if type(size) != int:
-            raise TypeError(" size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
