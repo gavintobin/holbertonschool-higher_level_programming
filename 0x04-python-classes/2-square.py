f20797b74a45d34ee0fa6f7177c0db97c2dcf56f
gavintobin <gavintobin714@gmail.com>
2023-01-16 18:49:53 -0800

    try1

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    "defines square class"
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """prints area"""
+        return self.__size ** 2
