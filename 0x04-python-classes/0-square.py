687bcc77a14282cc87212537cdc8d6caf794d2ac
gavintobin <gavintobin714@gmail.com>
2023-01-16 18:31:56 -0800

    try1

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Square class"""
+
+
 class Square:
     """defines square as class"""
     def __init__(self, size):
---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """defines square class"""
+    def __init__(self, size=0):
+        """initializes square with size"""
+        if type(size) != int:
+            raise TypeError(" size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
