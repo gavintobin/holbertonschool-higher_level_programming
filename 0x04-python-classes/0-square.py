12734a69d005eb7d1aeed51b675ec8b95a52f5e5
gavintobin <gavintobin714@gmail.com>
2023-01-16 18:21:59 -0800

    added comments

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Square class"""
 class Square:
+    """defines square as class"""
     def __init__(self, size):
+        """initializes square with size"""
         self.__size = size
