a29b3652bb0a88236442e7d6707b1956c4953a2b
gavintobin <gavintobin714@gmail.com>
2023-01-19 08:09:05 -0800

    syntax

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if a or b is not type(int):
-        raise TypeError('a must be an integer' / 'b must be an integer')
-    if a is type(float):
-        a = int(a)
-    if b is type(float):
-        b = int(b)
-        return a + b
+    if a is not type(int) or type(float):
+        raise TypeError('a must be an integer')
+    if b is not type(int) or type(float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
