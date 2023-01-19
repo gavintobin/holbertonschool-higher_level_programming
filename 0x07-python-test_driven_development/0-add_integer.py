930a4ad847dedf97a5a760a8170aac66986f1110
gavintobin <gavintobin714@gmail.com>
2023-01-19 08:00:13 -0800

    added new line eof

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     if a or b is not type(int):
-        raise TypeError('a must be an integer'/ 'b must be an integer')
+        raise TypeError('a must be an integer' / 'b must be an integer')
     if a is type(float):
         a = int(a)
     if b is type(float):
