c6520f4ac49c28eb793ebe481c6ddc28131c2dda
gavintobin <gavintobin714@gmail.com>
2023-01-24 12:59:47 -0800

    try1

---
+++ 0x06-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"rectangle"
+
+
+class Rectangle:
+    "rec class"
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+
+    def perimeter(self):
+        if width or height = 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rek = "#" * self.__width + "\n"
+        rek = rektrect * self.__height
+        rek = rektrect[:-1]
+        return rek
