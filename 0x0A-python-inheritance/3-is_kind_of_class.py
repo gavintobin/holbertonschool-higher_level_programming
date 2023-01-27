0c15f7f03f593ed1a91d0c10dfed6beafaffc70b
gavintobin <gavintobin714@gmail.com>
2023-01-27 07:39:35 -0800

    added comments

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""kindofclass"""
+
+
 def is_kind_of_class(obj, a_class):
+    "func"
     if isinstance(obj, a_class):
         return True
     if isinstance(obj, super(a_class)):
