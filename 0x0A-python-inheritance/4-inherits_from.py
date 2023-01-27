3d4375ad8ec1265ee3254b8d5ea6c29e0b8aa4ed
gavintobin <gavintobin714@gmail.com>
2023-01-27 07:45:39 -0800

    added comments

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """func"""
-    if type(obj)!= a_class and issubclass(type(obj, a_class)):
+    if type(obj) != a_class and issubclass(type(obj, a_class)):
         return True
-    return False 
+    return False
