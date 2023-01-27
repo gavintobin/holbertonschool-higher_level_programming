a013ad6a583638403d4a0eeb3d65998bbe2dd603
gavintobin <gavintobin714@gmail.com>
2023-01-27 08:23:44 -0800

    fixedd code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,8 +6,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """r tangs friend"""
     def __init__(self, size):
-        self.__size = size
         size.integer_validator = ("size", size)
-
-    def area(self):
-        return self.__size ** 2
+        super(),__init__(size, size)
+        self.__size = size
