5c187018a89b2f055b7fad51644e355cf508b6bd
gavintobin <gavintobin714@gmail.com>
2023-01-28 07:09:39 -0800

    try1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""write file"""
+
+
 def write_file(filename="", text=""):
+    """func"""
     with open(filename, encoding='utf-8') as f:
         f.write(text)
     return len(text)
