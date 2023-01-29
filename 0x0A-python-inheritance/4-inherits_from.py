cf749d83547fdf5b1708ba81d7eb5e556c1e0851
gavintobin <gavintobin714@gmail.com>
2023-01-28 16:46:56 -0800

    literally was missing bracket

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """func"""
-    return issubclass(type(obj, a_class)) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
