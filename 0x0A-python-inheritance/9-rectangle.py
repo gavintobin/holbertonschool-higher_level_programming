5a564559d2446ea6da33b571d54044a33c85f8df
gavintobin <gavintobin714@gmail.com>
2023-01-28 16:39:15 -0800

    fixed spelling

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-        self.integer_validation("width", width)
-        self.integer_validation("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def area(self):
         return self.__width * self.__height
