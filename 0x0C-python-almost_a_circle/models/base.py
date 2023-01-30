e154396e68dc54ad706fb2777c3d79d6b404975d
gavintobin <gavintobin714@gmail.com>
2023-01-30 05:34:53 -0800

    try1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Base:
+    _nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base._nb_objects += 1
+            self.id = Base._nb_objects
