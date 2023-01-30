9bb41127f5797f95bf1d2c0cd0335cc93d49ba9a
gavintobin <gavintobin714@gmail.com>
2023-01-30 07:29:11 -0800

    maybe now

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,86 +1,103 @@
 #!/usr/bin/python3
-"""inherited from base"""
+"""rectangle module"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """subclass of base"""
+    """rectangle subclass of base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         super().__init__(id)
 
-        @property
-        def width(self):
-            """retrieves the width"""
-            return self.__width
+    @property
+    def width(self):
+        """get width property of rectangle"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """sets width val for rectangle"""
-            if type(value) != int:
-                raise TypeError('width must be an integer')
-            if width <= 0:
-                raise ValueError('width must be > 0')
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """set width prop"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """gets height for rectangle"""
-            return self.__height
+    @property
+    def height(self):
+        """get height property of rectangle"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """sets height val for rectangle"""
-            if type(value) != int:
-                raise TypeError(' height must be an int')
-            if height <= 0:
-                raise ValueError('height must be > 0')
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """set height prop"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """gets x val for rectangle"""
-            return self.__x
+    @property
+    def x(self):
+        """get x property of rectangle"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """sets val of x in rectangle"""
-            if type(value) != int:
-                raise TypeError('x must be an integer')
-            if x < 0:
-                raise value
+    @x.setter
+    def x(self, value):
+        """set x prop"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """returns val of ractangle"""
-            return self.__y
+    @property
+    def y(self):
+        """get y property of rectangle"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """sets y val of rectangle"""
-            if type(value) != int:
-                raise TypeError('y must be an integer')
-            if y < 0:
-                raise ValueError('y must be >= 0')
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """set y prop"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
-        def area(self):
-            """ gets value of area for rec"""
-            return self.__height * self.__width
+    def area(self):
+        """area of rect is len * width"""
+        return self.width * self.height
 
-        def display(self):
-            """printts rectangle to stdout"""
-            r = '\n' * self.y
-            r += (' ' * self.x + '#' * self.width + '\n')
-            r = r[:-1]
-            print(r)
+    def display(self):
+        """prints rect to stdout"""
+        rektrect = '\n' * self.y
+        rektrect += (' ' * self.x + '#' * self.width + '\n') * self.height
+        rektrect = rektrect[:-1]
+        print(rektrect)
 
-        def __str__(self):
-            """returns string output of rec"""
-            return '[' + type(self.__name__) + ']' + '(' + str(self.__id) \
-                + ')' + str(self.__x) + '/' + str(self.__y) + '-' \
-                + str(self.__width) + '/' + str(self.__height)
+    def __str__(self):
+        """rets rect info"""
+        strekt = "[Rectangle] " + "({}) ".format(self.id)
+        strekt += "{}/{} - ".format(self.x, self.y)
+        strekt += "{}/{}".format(self.width, self.height)
+        return strekt
+
+    def update(self, *args, **kwargs):
+        """meth updates args"""
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for ii in range(len(args)):
+                setattr(self, attrs[ii], args[ii])
+        for ky, vl in kwargs.items():
+            setattr(self, ky, vl)
+
+    def to_dictionary(self):
+        """rets rect richard"""
+        richard = {"id": self.id, "width": self.width,
+                   "height": self.height, "x": self.x, "y": self.y}
+        return richard
