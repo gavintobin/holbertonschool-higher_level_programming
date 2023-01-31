5eaa3a0b4fa21e6c2f4bb8b4ee3b493617fb0182
gavintobin <gavintobin714@gmail.com>
2023-01-31 07:18:53 -0800

    added comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """subclass of rectangle"""
 
     def __init__(self, size, x=0, y=0, kid=None):
-        super().__init__(id, x, y)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
