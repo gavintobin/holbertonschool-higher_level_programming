13c80ee9c2cf4a4e3ae80872f82e576c989b7989
gavintobin <gavintobin714@gmail.com>
2023-01-31 07:22:33 -0800

    maybe now

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""square class"""
+"""square module"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """subclass of rectangle"""
+    """square subclass of rectangle"""
 
-    def __init__(self, size, x=0, y=0, kid=None):
+    def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """gets size of square"""
+        """get size property of square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """sets value of size for sq"""
+        """set size prop"""
         self.width = value
         self.height = value
 
     def __str__(self):
         """rets sqr info"""
-        r = "[Square] " + "({}) ".format(self.id)
-        r += "{}/{} - ".format(self.x, self.y)
-        r += "{}".format(self.width)
-        return r
+        strekt = "[Square] " + "({}) ".format(self.id)
+        strekt += "{}/{} - ".format(self.x, self.y)
+        strekt += "{}".format(self.width)
+        return strekt
