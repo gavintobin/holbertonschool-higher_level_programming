ac75321fd527ca2300d74f11f79f13f0591a7582
gavintobin <gavintobin714@gmail.com>
2023-02-09 12:25:46 -0800

    try1

---
+++ 0x00-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- computes score average of records in table 2
+SELECT AVG(score) AS average
+FROM second_table;
+
---
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- lists all records of table
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC, name DESC;
+
