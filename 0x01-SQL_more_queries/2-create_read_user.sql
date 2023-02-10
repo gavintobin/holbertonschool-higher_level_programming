21dd28ae6da54e83750fcefbc5fcf9117615479e
gavintobin <gavintobin714@gmail.com>
2023-02-10 10:20:39 -0800

    try1

---
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- creates new db and user
+IF NOT EXISTS CREATE DATABASE hbtn_0d_2;
+IF NOT EXISTS CREATE USER 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_0d_2
+TO user_0d_2@localhost;
+
+
