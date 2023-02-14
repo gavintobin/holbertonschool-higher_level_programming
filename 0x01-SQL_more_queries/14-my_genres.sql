ca8897c7dbe58792edd2cdb5b7cc9c72ea84b023
gavintobin <gavintobin714@gmail.com>
2023-02-14 10:02:40 -0800

    try1

---
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- lists all genres of show dexter
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
+
