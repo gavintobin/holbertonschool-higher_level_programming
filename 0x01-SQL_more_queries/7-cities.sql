764248b6ae1c1bc3d1045e0ac3e98b8a6d3e82c1
gavintobin <gavintobin714@gmail.com>
2023-02-14 06:28:02 -0800

    try1

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL
-	PRIMARY_KEY (id),
-	UNIQUE(id),
-	FOREIGN KEY (state_id) REFERENCE states(id));
+	PRIMARY KEY (id),
+	UNIQUE (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	);
 
