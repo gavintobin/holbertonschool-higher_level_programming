bf4a04b52897197398d0a50bb040576b497bdade
gavintobin <gavintobin714@gmail.com>
2023-02-14 06:12:06 -0800

    try1

---
+++ 0x01-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates database and table
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+USE 'hbtn_0d_usa';
+
+CREATE TABLE IF NOT EXISTS 'states' (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(256)
+	PRIMARY KEY(id)
+	);
