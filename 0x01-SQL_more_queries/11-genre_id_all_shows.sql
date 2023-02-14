1eae37863fd89d018d8abab473c5c852923b7ef8
gavintobin <gavintobin714@gmail.com>
2023-02-14 08:10:42 -0800

    try1

---
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows containd in tv show db
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_shows_genre
+ON tv_shows.id = tv_shows_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
