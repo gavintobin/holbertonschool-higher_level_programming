2d670bf3b7823f347169d9a69ed87187a76e8f0f
gavintobin <gavintobin714@gmail.com>
2023-02-14 08:21:26 -0800

    try1

--- 0x01-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genre
 ON tv_shows.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
