a3bbb570d5a0e2f583b8858ab8b4b9c9807fd552
gavintobin <gavintobin714@gmail.com>
2023-02-14 09:54:50 -0800

    try1

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,7 @@
 -- lists all genres and displays shows linked to 
-SELECT tv_genres.name, 
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
