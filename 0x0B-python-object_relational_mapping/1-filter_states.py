34285d5765987d8cf2969ed258c840ed0673ffcc
gavintobin <gavintobin714@gmail.com>
2023-02-23 12:30:17 -0800

    try1

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all states starting with n"""
+import MySQLdb
+import sys
+
+
+def list_all():
+    """list all function"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("Select * FROM states WHERE name  LIKE 'N%'   ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    list_all()
