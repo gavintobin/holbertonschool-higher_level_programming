2e000d370c3200cc8d52f0530ab081bf1737b82e
gavintobin <gavintobin714@gmail.com>
2023-02-23 12:12:06 -0800

    fixedv arg

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """lists all states"""
 import MySQLdb
-from  sys import argv
+import sys
 
 
 def list_all():
     """list all function"""
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.arg[1],
-                           passwd=sys.arg[2], db=sys.arg[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("Select * FROM states  ORDER BY id ASC")
     query_rows = cur.fetchall()
