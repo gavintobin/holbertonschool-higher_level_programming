da7c77a3a4db7896e64ea54caa049e059d4e5a1d
gavintobin <gavintobin714@gmail.com>
2023-02-24 10:43:01 -0800

    try1

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,9 @@ def list_all():
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("Select name FROM cities  WHERE state_id = ANY ( SELECT id \
+    cur.execute("Select name FROM cities WHERE state_id = (SELECT id \
                  FROM states \
-                 WHERE BINARY name = %(state)s) \
+                 WHERE  name = %(state)s) \
                  ORDER BY id ASC", {'state': sys.argv[4]})
     query_rows = cur.fetchall()
     for row in query_rows:
