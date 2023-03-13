20e3091bfce1793b95045a2f7c65e4c9cac25062
gavintobin <gavintobin714@gmail.com>
2023-03-13 06:58:14 -0700

    try1

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -11,7 +11,7 @@ the body of the response (decoded in utf-8)
 if __name__ == '__main__':
     import requests
     req = requests.get('https://intranet.hbtn.io/status')
- 
+
     print('Body response:')
     print('\t- type:', type(req.text))
     print('\t- content:', req.text)
