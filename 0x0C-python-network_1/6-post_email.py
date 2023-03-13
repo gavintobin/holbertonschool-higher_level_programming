a6fa142a9ba5759d5e1ddc3a522efca70ec1fbf0
gavintobin <gavintobin714@gmail.com>
2023-03-13 07:11:51 -0700

    try1

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+The email must be sent in the variable email
+"""
+import requests
+import sys
+
+
+url = sys.argv[1]
+email = sys.argv[2]
+data_req = requests.post(url, {"email": email})
+
+print(data_req.text)
