3931fc2b71a0776a0505f110dac51ed84be3cd5f
gavintobin <gavintobin714@gmail.com>
2023-03-13 06:29:24 -0700

    fixed spellling

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
 
-    print('Body Response:')
+    print('Body response:')
     print('\t- type:', type(html))
     print('\t- content:', html)
     print('\t- utf8 content:', html.decode('utf-8'))
