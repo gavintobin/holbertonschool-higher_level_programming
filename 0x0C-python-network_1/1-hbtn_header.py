5a95dac673af449d73e17d0e5c783f80c8694451
gavintobin <gavintobin714@gmail.com>
2023-03-13 07:56:01 -0700

    doc1

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""
-header task 1Write a Python script
-that takes in a URL, sends a request
-to the URL and displays the value of
-the X-Request-Id variable found in the
-header of the response.
-"""
-import sys
+"""HBTN Status"""
 import urllib.request
+import sys
 
-if __name__ == '__main__':
-
-    url = sys.argv[1]
-
-with urllib.request.urlopen(url) as resp:
-    head = resp.info()
-    x_request_id = head['X-request-Id']
-    print(x_request_id)
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.headers['X-Request-Id'])
