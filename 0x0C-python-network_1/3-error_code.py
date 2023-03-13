043a13728532afdac6d3a4048cc546bfbbb05cfc
gavintobin <gavintobin714@gmail.com>
2023-03-13 07:53:07 -0700

    doc3

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-u hoh error code Write a Python script that takes
+error code Write a Python script that takes
 in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the
 header of the response.
