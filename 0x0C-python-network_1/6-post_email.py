2b1e718e9e664b1dff1e52829f578a1565853288
gavintobin <gavintobin714@gmail.com>
2023-03-13 07:17:11 -0700

    doc error

--- 0x0C-python-network_1/6-post_email.py
+++ 0x0C-python-network_1/6-post_email.py
@@ -8,9 +8,9 @@ The email must be sent in the variable email
 import requests
 import sys
 
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data_req = requests.post(url, {"email": email})
 
-url = sys.argv[1]
-email = sys.argv[2]
-data_req = requests.post(url, {"email": email})
-
-print(data_req.text)
+    print(data_req.text)
