58ac15f82e4a03bcea3cc175f94db87cc7438a6a
gavintobin <gavintobin714@gmail.com>
2023-03-13 06:52:56 -0700

    try1

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -6,14 +6,12 @@ a POST request to the passed URL with
 the email as a parameter, and displays
 the body of the response (decoded in utf-8)
 """
-from urllib import request
 
 
 if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    import requests
+    req = requests.get('https://intranet.hbtn.io/status')
  
     print('Body response:')
-    print('\t- type:', type(html))
-    print('\t- content:', html)
+    print('\t- type:', type(req.text))
+    print('\t- content:', req.text)
