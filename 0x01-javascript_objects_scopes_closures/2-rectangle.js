3bc478a8dd9b2006520c66432178ba649c7acdde
gavintobin <gavintobin714@gmail.com>
2023-04-11 13:30:59 -0700

    try1

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0) {
         // create an empty object if either w or h is 0 or negative
