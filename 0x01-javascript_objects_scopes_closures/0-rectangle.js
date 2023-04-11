f132467bbf9c8c43620640b818ffe88d953611b7
gavintobin <gavintobin714@gmail.com>
2023-04-11 07:36:05 -0700

    try1

--- 0x01-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-module.exports = class rectangle {};
+module.exports = class Rectangle {};
--- 0x01-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
