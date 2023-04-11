ad22dee89c98fa17fdd2bd30a2322bc7cf1e0853
gavintobin <gavintobin714@gmail.com>
2023-04-11 06:14:02 -0700

    try1

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial(n) {
+    if (isNaN(n)) {
+      return 1;
+    } else if (n == 0) {
+      return 1;
+    } else {
+      return n * factorial(n-1);
+    }
+  }
+  
+  const num = parseInt(process.argv[2]);
+  
+  console.log(factorial(num));
