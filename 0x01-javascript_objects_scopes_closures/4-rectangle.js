e4c8237e57bc86ddcf662c722dd64a3add870893
gavintobin <gavintobin714@gmail.com>
2023-04-11 13:51:45 -0700

    try2

---
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}; // create an empty object if w or h is not a positive integer
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let rectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rectangle += 'X';
+      }
+      rectangle += '\n';
+    }
+    console.log(rectangle);
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
