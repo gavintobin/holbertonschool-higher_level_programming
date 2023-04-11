f07733d51cf747dfa8e589a0e32beca6ac941661
gavintobin <gavintobin714@gmail.com>
2023-04-11 13:42:41 -0700

    try1

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,4 +7,18 @@ module.exports = class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    if (!this.width || !this.height) {
+      console.log('Empty Rectangle');
+      return;
+    }
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
 };
