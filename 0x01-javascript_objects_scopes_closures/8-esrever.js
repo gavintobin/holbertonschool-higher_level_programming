e68cf76ec9b314fc90583408535384ee83ed97a6
gavintobin <gavintobin714@gmail.com>
2023-04-11 19:50:58 -0700

    try1

--- 0x01-javascript_objects_scopes_closures/8-esrever.js
+++ 0x01-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    var i =0; j= list.length-1;
-    while(i < j) {
-      var temp = list[i];
-      list[i] = list[j];
-      list[j] = temp;
-      i++;
-      j--;
-    }
-    return list;
+  var i =0; j= list.length-1;
+  const reversedList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
   }
+
+  return reversedList;
+};
