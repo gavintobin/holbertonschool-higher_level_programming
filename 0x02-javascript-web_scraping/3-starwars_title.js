c18dfcec0332dade729883b8babba809cbde5d21
gavintobin <gavintobin714@gmail.com>
2023-04-14 12:53:16 -0700

    try1

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 
 const movie = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+const url = 'https://swapi-api.hbtn.io/api/films/' + movie;
 
 request.get(url, function (err, response, body) {
     if (err) throw err;
