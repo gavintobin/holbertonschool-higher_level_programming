d97635988093f438fdd61b89377695240cd32195
gavintobin <gavintobin714@gmail.com>
2023-04-14 09:59:01 -0700

    try1

---
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movie = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+request.get(url, function (err, response, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).title);
+});
